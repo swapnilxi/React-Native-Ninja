ce01ef2b937bf4ae62afef30ae2b64b967b3414b
swapnil <swapnil@filmboardmovies.com>
2023-09-30 21:36:10 +0530

    adding React-native-notifier

---
+++ Notifier-app/notification-react-native-backend/src/main/java/com/swapnilxi/notificationreactnativebackend/NotificationReactNativeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swapnilxi.notificationreactnativebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationReactNativeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationReactNativeBackendApplication.class, args);
+	}
+
+}
